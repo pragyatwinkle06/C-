47a6bd48e530be70d56b1d8b0afd4150136b882a
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2022-02-07 13:31:44 +0530

    Add files via upload

---
+++ hextod.cpp
@@ -0,0 +1,36 @@
+//hexa to decimal
+#include<bits/stdc++.h>
+using namespace std ;
+
+
+
+int hex_to_dec(string n)
+{
+    int ans=0;
+    int x=1;
+    int s=n.size();
+   for(int i=s-1; i>=0;i--)
+    {
+        if(n[i] >= '0' && n[i]<= '9')
+        {
+            ans =ans +  x*(n[i] - '0');
+        }
+
+        else if(n[i] >= 'A' && n[i]<= 'F')
+       {
+        ans  += x*(n[i] - 'A' + 10);
+       } 
+      x=x*16;
+      
+    }
+    return ans;
+}
+int main()
+{
+    string num;
+    cout<<"Enter Hexa  value\n";
+    cin>>num;
+    cout<<hex_to_dec(num);
+    return 0;
+
+}
