77ee852fde572c676893ae57df4dcdee5d848900
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2022-02-09 12:21:08 +0530

    Add files via upload

---
+++ subarray.cpp
@@ -0,0 +1,30 @@
+//all  possible subarray sum
+#include<bits/stdc++.h>
+using namespace std;
+ int main()
+ {
+      int n;
+    cout<<"Enter size\n";
+    cin>>n;
+   
+    int arr[n];
+    for (int i = 0; i< n; i++)
+    {
+        cin>>arr[i];
+        
+    }
+int sum=0;
+    for(int i=0 ; i<n;i++)
+    {    sum=0;
+        for(int j=i ; j<n; j++)
+        {
+          sum+=arr[j];
+          cout<<sum<<endl;
+        }
+       
+       
+    }
+  
+
+    return 0;
+ }
