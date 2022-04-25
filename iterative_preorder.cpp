9428b82dd6e4ba20c3968f8ae346a9da717f00cd
Pragya Shukla <56453802+pragyatwinkle06@users.noreply.github.com>
2022-04-25 18:05:12 +0530

    Add files via upload

---
+++ iterative_preorder.cpp
@@ -0,0 +1,25 @@
+//Iterative Preorder Traversal 
+vector<int> preorder_T(TreeNode* root)
+{
+    vector<int> preorder;
+    if(root == NULL)
+    return preorder;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while(!st.empty())
+    {
+        root=st.top();
+        st.pop();
+        preorder.push_back(root->val);
+        if(root->right !=NULL)
+        st.push(root->right);
+
+        if(root->left!=NULL)
+        st.push(root->left);
+
+        
+    }
+    return preorder;
+}
